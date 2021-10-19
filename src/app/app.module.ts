d021a9fc6beebe8342091c58184f21ce2c4bdd6a
Nitha <alive1704@gmail.com>
2021-10-20 03:43:14 +0700

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { PolicyComponent } from './pages/policy/policy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PolicyComponent
   ],
   imports: [
     BrowserModule,
